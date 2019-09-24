89134e91599bb8eb95915ca063dd27b2daa0cf4a
Bilal Ahmed <bill.ahmed@mail.utoronto.ca>
2019-09-24 18:04:20 -0400

    added nativebase library support

--- client/App.tsx
+++ client/App.tsx
@@ -1,12 +1,44 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import { AppLoading } from 'expo';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Font from 'expo-font';
+import { Ionicons } from '@expo/vector-icons';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
-  );
+  // Equivalent to this.state
+  const [isReady, setReady] = useState(false);
+
+  // Equivalent to componentDidMount()
+  useEffect(() => {
+    // Load Fonts used for NativeBase
+    Font.loadAsync({
+      Roboto: require('./resources/Fonts/Roboto.ttf'),
+      Roboto_medium: require('./resources/Fonts/Roboto_medium.ttf'),
+      ...Ionicons.font,
+    });
+
+    // App is ready to initialize
+    setReady(true);
+  });
+
+  if(isReady){
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.tsx to start working on your app!</Text>
+      </View>
+    );
+  } else{
+    return (
+      <View style={styles.container}>
+
+      </View>
+    );
+  }
+  // return (
+  //   <View style={styles.container}>
+  //     <Text>Open up App.tsx to start working on your app!</Text>
+  //   </View>
+  // );
 }
 
 const styles = StyleSheet.create({
