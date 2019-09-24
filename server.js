9a79d9508d727df5b58d37ab276d942761fa1757
Bilal Ahmed <bill.ahmed@mail.utoronto.ca>
2019-09-23 22:30:57 -0400

    firebase-admin SDK and service account credentials

--- server.js
+++ server.js
@@ -1,12 +1,29 @@
+import * as admin from 'firebase-admin';
+
 const EXPRESS = require('express'); // server
 const CORS = require('cors');
 
 const APP = EXPRESS();  // our app instance
 const PORT = 4000;  // Port to run the application on
 
+
+/**     SETUP VARIABLES AND OTHER REQUIRED DATA     **/
+var serviceAccount = require("./og-planit-firebase-serviceAccountKey.json"); // Private key to connect to Firebase services
+
+// Initialize SDK
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://og-planit.firebaseio.com"
+});
+
+
+/****     INITIALIZE SERVER       ****/
+
 // Enable CORS
 APP.use(CORS());
 
+
+/**     API ROUTES      **/
 
 /**Root page */
 APP.get('/', (req, res) => {
