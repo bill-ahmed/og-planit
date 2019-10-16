148debc4f88679ad250040574b87b92a75750783
Bilal Ahmed <bill.ahmed@mail.utoronto.ca>
2019-10-16 15:56:22 -0400

    typescript support for backend

--- server.ts
+++ server.ts
@@ -1,3 +1,4 @@
+import Routes from './utils/routes';
 const admin = require('firebase-admin');
 const bodyParser = require('body-parser');
 
@@ -24,7 +25,7 @@ admin.initializeApp({
 //APP.use(CORS());
 
 /* Enabls CORS */
-APP.use(function(req, res, next) {
+APP.use(function(req: any, res: any, next: any) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
@@ -34,38 +35,14 @@ APP.use(function(req, res, next) {
 /****     API ROUTES      ****/
 
 /**Root page */
-APP.get('/', (req, res) => {
+APP.get('/', (req: any, res: any) => {
     // Template for simple get request.
     res.send('Hello World!')
 });
 
 
 /** Create new user **/
-APP.post('/createUser', bodyParser.json(), (req, res) => {
-    console.log(req.body);
-    res.header("Content-Type", "application/json");
-    
-    // Make a request to firebase to create a user
-    admin.auth().createUser({
-        email: req.body.email,
-        password: req.body.password,
-        emailVerified: false,
-        displayName: `${req.body.firstName} ${req.body.middleName} ${req.body.lastName}`,
-        photoURL: req.body.photoURL,
-        disabled: false,
-        age: req.body.age
-    })
-    .then(userRecord => {
-        console.log(`Created new user with ID: ${userRecord.uid}`)
-        res.statusCode = 200;   // 200 = okay
-        res.json(userRecord);   // Return the response back
-    })
-    .catch(error => {
-        console.log(`Error creating user: ${error}`);
-        res.statusCode = 400;   // 400 = user error caused it to fail...probably
-        res.json(error);    // Return the response back
-    });
-});
+APP.post('/createUser', bodyParser.json(), (req: any, res: any) => Routes.createUserPOST(req, res));
 
 
 APP.listen(PORT, () => {
---
+++ utils/routes.ts
@@ -0,0 +1,32 @@
+/**Define API routes for Express.js back-end **/
+
+export default class Routes{
+
+    /**Create a new user in Firebase and provision space in Firestore */
+    public static createUserPOST(req: any, res: any){
+        console.log(req.body);
+        res.header("Content-Type", "application/json");
+        
+        // Make a request to firebase to create a user
+        admin.auth().createUser({
+            email: req.body.email,
+            password: req.body.password,
+            emailVerified: false,
+            displayName: `${req.body.firstName} ${req.body.middleName} ${req.body.lastName}`,
+            photoURL: req.body.photoURL,
+            disabled: false,
+            age: req.body.age
+        })
+        .then(userRecord => {
+            console.log(`Created new user with ID: ${userRecord.uid}`)
+            res.statusCode = 200;   // 200 = okay
+            res.json(userRecord);   // Return the response back
+        })
+        .catch(error => {
+            console.log(`Error creating user: ${error}`);
+            res.statusCode = 400;   // 400 = user error caused it to fail...probably
+            res.json(error);    // Return the response back
+        });
+    }
+}
+
