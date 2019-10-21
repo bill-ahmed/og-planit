263528b4b665739179ea8e3860f7b78ea692d576
liborui1 <borui.li@mail.utoronto.ca>
2019-10-21 11:32:22 -0400

    added provision new events, and added ratongs to user itineraires

--- utils/routes.ts
+++ utils/routes.ts
@@ -77,9 +77,81 @@ export default class Routes{
         // Also need to store collection of itineraries for this user
         let itineraries = this.db.collection('dev').doc('data').collection('users').doc(uid).collection('itineraries').doc('INIT_ITINERARY');
         itineraries.set({
-            id: null, type: null, price: null, location: null, time: null, referenceToOriginal: null, last_edit_time: null  // Firebase requires at least ONE doc per collection
+            id: null, type: null, price: null, location: null, time: null, rating: 0, referenceToOriginal: null, review:"", last_edit_time: null  // Firebase requires at least ONE doc per collection
         });
         
     }
+
+    /**Create a new event in Firebase and provision space in Firestore */
+    public createEventPOST(req: any, res: any){
+        console.log(req.body);
+        res.header("Content-Type", "application/json");
+
+        /** Make a request to firebase to create a event **/
+        const eventInformation = {
+            Address: `${req.body.City} ${req.body.Country} ${req.body.Number} ${req.body.Province} ${req.body.Street}`,
+            AvgPrice: req.body.AvgPrice,
+            AvgTimeSpent: req.body.AvgTimeSpent,
+            ContactInfo: `${req.body.Email} ${req.body.Phone}`,
+            Description: req.body.Description,
+            EndTime: req.body.EndTime,
+            Location: req.body.Location,
+            Name: req.body.Name,
+            Ratings: `${req.body.AveRating} ${req.body.NumRatings}`,
+            StartTime: req.body.StartTime,
+			Type: req.body.Type
+        };
+
+        this.admin.auth().createUser(eventInformation)
+        .then((eventRecord: any) => {
+            console.log(`Created new Event with ID: ${eventRecord.uid}`);
+
+            // Provision this new event a document in our db
+            try {
+                this.provisionNewUser(eventInformation, eventRecord.uid);
+
+                res.statusCode = 200;   // 200 = okay
+                res.json(eventRecord);   // Return the response back
+
+            } catch (error) {
+                console.log(error);
+                res.statusCode = 500;   // Internal error with Firestore
+                res.json(error)
+            }
+        })
+        .catch((error: any) => {
+            console.log(`Error creating event: ${error}`);
+            res.statusCode = 400;   // 400 = event error caused it to fail...probably
+            res.json(error);    // Return the response back
+        });
+
+    }
+
+    /**Provision a new event into Firestore
+     * @param eventData An object containing address, name, description, ect.
+     * @param eid The unique ID of this new event
+     */
+    private provisionNewEvent(eventData: any, eid: string){
+        /** Create document for this new event **/
+        let eventInfo = {
+            Address: [eventData.City, eventData.Country, eventData.Number, eventData.Province, eventData.Street],
+            AvgPrice: eventData.AvgPrice,  
+            AvgTimeSpent: eventData.AvgTimeSpent,
+            ContactInfo: [eventData.Email, eventData.Phone ],
+            Description:  eventData.Description, 
+			EndTime:  eventData.EndTime, 
+			Location:  eventData.Location, 
+			Name:  eventData.Name,
+			Ratings: [eventData.AveRating, eventData.NumRatings ],
+			StartTime:  eventData.StartTime, 
+			Type:  eventData.Type, 
+            Tags: {}
+        };
+
+        // Commit new document
+        let newUserInfoRef = this.db.collection('dev').doc('data').collection('events').doc(eid);
+        newUserInfoRef.set(eventInfo);
+    }
+
 }
 
