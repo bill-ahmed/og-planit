3b1333db637b00995e6f59245f9e55b0714f1a0c
Aschente <48228100+Aschente0@users.noreply.github.com>
2019-11-02 16:09:17 -0400

    created card views for events

--- client/app/source/itinerary/components/card_list_views_events/CreateViews.tsx
+++ client/app/source/itinerary/components/card_list_views_events/CreateViews.tsx
@@ -3,10 +3,15 @@ import {Container, Header, Left, Right, Body, Title, Subtitle, Content, Button, 
 import {Rating} from 'react-native-ratings';
 
 import styles from './CreateViewsStyles';
-import { View, Text, ScrollView, Dimensions } from 'react-native';
+import { View, Text, ScrollView, Dimensions, Modal, TouchableHighlight } from 'react-native';
+import { Card, ListItem} from 'react-native-elements';
 import Image from 'react-native-scalable-image';
 import { EventEmitter } from '@unimodules/core';
+import { array } from 'prop-types';
+import { database } from 'firebase';
 export default function CreateViews(props){
+
+    const json = require("../ratings/mockDatabase.json");
 
     return(
         <Container>
@@ -26,6 +31,24 @@ export default function CreateViews(props){
                 </Body>
                 <Right/>
             </Header>
+            <ScrollView>
+                {json.map(event => {
+                    return(
+                        <Card
+                            image={{uri: event.uri}}>
+                            <Text style={styles.eventHeader}>
+                                {event.Name}
+                            </Text>
+                            <Text>
+                                {"$" + event.AvgPrice + "\n"}
+                                {event.Address.Number + " " + event.Address.Street + ", " + event.Address.City + ", " + event.Address.Province + "\n"}
+                                {event.StartTime + " - " + event.EndTime}
+                            </Text>
+
+                        </Card>
+                    )
+                    })}
+            </ScrollView>
 
         </Container>
     );
