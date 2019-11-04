b1a8021fbcbc1085f75f3a397f694db02e66cf10
lin432 <yinlintao@gmail.com>
2019-11-03 20:02:31 -0500

    itineraryAPI completed with stack changes

--- client/app/source/itinerary/api/itineraryAPI.tsx
+++ client/app/source/itinerary/api/itineraryAPI.tsx
@@ -1,9 +1,11 @@
 import { Itinerary } from "../models/location";
 import { useSelector } from "react-redux";
-
 const firebase = require("firebase");
 
-export async function getLocations(): Promise<Itinerary[]> {
+// Required for side-effects
+require("firebase/firestore");
+
+export async function getItinerarySigned(): Promise<Itinerary[]> {
     let startingCollection = 'prod';
     // If in dev environment, grab from dev db
     if(__DEV__){
@@ -12,17 +14,18 @@ export async function getLocations(): Promise<Itinerary[]> {
     // Reference to firestore db
     var db = firebase.firestore();
     const uid = useSelector(state => state['UserInfo']['uid']);
-    
-    return new Promise<Itinerary[]>((resolve, reject) => {
-        db.collection(startingCollection).doc('data').collection("users").doc(uid).collection("itineraries").get().then((querySnapshot:any) => {
-            const arr = [];
 
-            querySnapshot.forEach(doc => arr.push(doc.data()));
-            resolve(arr);
-        })
-        .catch((err:any) => {
-            console.log("Error getting document", err);
-            reject(err);
-        });
+    return new Promise<Itinerary[]>((resolve, reject) => {
+        db.collection(startingCollection).doc('data').collection('users').doc(uid).collection('itineraries').get().then((querySnapshot:any) => {
+            
+                const arr = [];
+    
+                querySnapshot.forEach(doc => arr.push(doc.data()));
+                resolve(arr);
+            })
+            .catch((err:any) => {
+                console.log("Error getting document", err);
+                reject(err);
+            });
     });
 }
